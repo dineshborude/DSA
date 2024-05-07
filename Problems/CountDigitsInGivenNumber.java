2f29f1bd4fdbe5134a340b0bf522d53370c941c0
kiran-vishwakarma <verti_kiran_vishwakarma@golfdistrict.com>
2024-05-07 21:26:08 +0530

    Dealing with Numbers

---
+++ Problems/CountDigitsInGivenNumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class CountDigitsInGivenNumber {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Desired Number : ");
+
+        int num = sc.nextInt();
+
+        System.out.println("Number : " +num);
+
+        System.out.println("Total Digits In Number Are : " +countDigit(num));
+
+
+    }
+
+    public static int countDigit(int num) {
+        int count = 0;
+        while (num != 0) {
+            num = num / 10; // Divide num by 10 to remove one digit
+            count++; // Increment the count for each digit removed
+        }
+        return count;
+    }
+
+
+}
+
---
+++ Problems/ReverseGivenNumber.java
@@ -0,0 +1,18 @@
+public class ReverseGivenNumber {
+    public static void main(String[] args) {
+        int number = 12345;
+        int reversedNumber = reverse(number);
+        System.out.println("Original number: " + number);
+        System.out.println("Reversed number: " + reversedNumber);
+    }
+
+    public static int reverse(int number) {
+        int reversedNumber = 0;
+        while (number != 0) {
+            int digit = number % 10; // Extract rightmost digit
+            reversedNumber = reversedNumber * 10 + digit; // Append the digit to reversedNumber
+            number /= 10; // Remove the rightmost digit
+        }
+        return reversedNumber;
+    }
+}
