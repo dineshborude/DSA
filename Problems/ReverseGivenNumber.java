94bbe9734ba36e9d20952a9b2f580a0914f60615
Dinesh Borude <borudedinesh2@gmail.com>
2024-05-12 03:50:49 +0530

    Reverse Number With Formula

--- Problems/ReverseGivenNumber.java
+++ Problems/ReverseGivenNumber.java
@@ -8,7 +8,7 @@ public class ReverseGivenNumber {
 
         int num = sc.nextInt();
 
-        String reversedNumber = reverse(num);
+        int reversedNumber = reverseMath(num);
         System.out.println("Original number: " + num);
         System.out.println("Reversed number : " + reversedNumber);
     }
@@ -20,6 +20,17 @@ public class ReverseGivenNumber {
             lastDigit = number % 10;
             number = number / 10;
             reversedNumber += lastDigit;
+        }
+        return reversedNumber;
+    }
+
+    public static int reverseMath(int number) {
+        int reversedNumber = 0;
+        int lastDigit = 0;
+        while (number > 0) {
+            lastDigit = number%10;
+            reversedNumber = (reversedNumber*10)+lastDigit;
+            number=number/10;
         }
         return reversedNumber;
     }
