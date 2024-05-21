87d655a38f8191c47403d89034d0fb5efd00aa7d
Dinesh Borude <borudedinesh2@gmail.com>
2024-05-22 02:23:07 +0530

    OccuranceOfCharacter Done

---
+++ Problems/MaxNumberInArray.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class MaxNumberInArray {
+
+    public static void main(String[] args) {
+        int[] num = {13,4,7,18,9,2};
+        System.out.println(Arrays.toString(num));
+        int length = num.length;
+        System.out.println("Length Of Given Array : "+length);
+        int max = 0;
+        
+        for (int i = 0; i < length; i++) {
+            max = num[i];
+            for (int j = 0; j < length; j++) {
+                
+                if (max <= num[j]) {
+                    max = num[j];
+                }
+                
+            }
+            
+        }
+        System.out.println("Maximum In Array : "+max);
+    }
+
+
+
+
+}
