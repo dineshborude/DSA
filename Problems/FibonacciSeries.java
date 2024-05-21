b03638cd7b0814a76cea7da8d6441a503813a471
Dinesh Borude <borudedinesh2@gmail.com>
2024-05-22 01:18:07 +0530

    Fibbonacci

--- Problems/FibonacciSeries.java
+++ Problems/FibonacciSeries.java
@@ -1,2 +1,20 @@
 public class FibonacciSeries {
+
+    public static void main(String[] args) {
+        int N1 = 0;
+        int N2= 1;
+        int N3;
+        int count = 10;
+        System.out.print(N1 +", " +N2);
+        for (int i = 2; i < count; i++) {
+
+            N3 = N1 + N2;
+            System.out.print(", " +N3);
+            N1 = N2;
+            N2 = N3;
+
+        }
+    }
+
+
 }
