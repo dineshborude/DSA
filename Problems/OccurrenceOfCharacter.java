4bf4594b3daf7234718893a2c9961b177f3cd6f3
Dinesh Borude <borudedinesh2@gmail.com>
2024-05-22 01:53:47 +0530

    OccuranceOfCharacter Done

--- Problems/OccurrenceOfCharacter.java
+++ Problems/OccurrenceOfCharacter.java
@@ -3,6 +3,25 @@ public class OccurrenceOfCharacter {
     public static void main(String[] args) {
 
         String given = "Hello World";
+        given = given.replaceAll(" ","");
+        System.out.println(given);
+        int length = given.length();
+        System.out.println("Length Of Given String : " +length);
+        int count = 0;
+
+        for (int i = 0; i < length; i++) {
+
+            for (int j = 0; j < length; j++) {
+
+                if (given.charAt(i) == given.charAt(j)){
+                    count++;
+                }
+
+            }
+            System.out.println("Count Of "+given.charAt(i) +" is "+count);
+            count = 0;
+        }
+
 
 
     }
