4f396e9a70b83525979d0e9b8b9d896a207ca9da
Dinesh Borude <borudedinesh2@gmail.com>
2024-05-24 23:53:10 +0530

    Duplio

--- Problems/RemoveRepeatedCharacters.java
+++ Problems/RemoveRepeatedCharacters.java
@@ -1,5 +1,26 @@
 public class RemoveRepeatedCharacters {
 
+    public static void main(String[] args) {
 
+        String str = "Hello Mama";
+        String str2 = "";  // Use a String to store the result
 
+        int len = str.length();
+
+        for (int i = 0; i < len; i++) {
+            boolean isRepeated = false;
+            for (int j = 0; j < i; j++) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    isRepeated = true;
+                    break;
+                }
+            }
+            if (!isRepeated) {
+                str2 += str.charAt(i);  // Concatenation creates new String objects
+            }
+        }
+
+        System.out.println("Original String: " + str);
+        System.out.println("String without repeated characters: " + str2);
+    }
 }
