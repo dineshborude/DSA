4cb8bd433af07d0bc55ea7dceb718deaa78d690d
Dinesh Borude <borudedinesh2@gmail.com>
2024-05-30 00:55:24 +0530

    Triangle & Pyramid Done

--- Problems/patterns/Pyramid.java
+++ Problems/patterns/Pyramid.java
@@ -1,23 +1,25 @@
 package patterns;
 
-public class Triangle {
+public class Pyramid {
 
     public static void main(String[] args) {
         int rows = 5;
 
+        for(int i = 1; i <= rows; i++) {
 
-        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows-i; j++) {
 
-            for (int j = 1; j <= rows - i; j++) {
                 System.out.print("  ");
-            }
 
-            for (int k = 1; k <= i; k++) {
+            }
+            for (int k = 1; k <= (2*i-1); k++) {
+
                 System.out.print("* ");
+
             }
             System.out.println();
         }
 
-
     }
+
 }
