f2c9370d0460b2a3bf77ec1fd78d5e0733362596
Dinesh Borude <borudedinesh2@gmail.com>
2024-06-18 23:58:33 +0530

    Still Working on it

--- Problems/patterns/Practise_Triangle.java
+++ Problems/patterns/Practise_Triangle.java
@@ -1,4 +1,17 @@
 package patterns;
 
 public class Practise_Triangle {
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 5; i++) {
+
+            for(int j = 0; j < 5; j++) {
+
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+
 }
