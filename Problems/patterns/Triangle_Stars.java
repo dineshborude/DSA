dc32fd234da91482999fff199a87451a53f6a3ea
Dinesh Borude <borudedinesh2@gmail.com>
2024-06-19 02:07:04 +0530

    Still Working on it

---
+++ Problems/patterns/Triangle_Stars.java
@@ -0,0 +1,19 @@
+package patterns;
+
+public class Triangle_Stars {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+
+            for (int j = 0; j < i+1; j++) {
+
+                System.out.print(" * ");
+
+            }
+            System.out.println();
+        }
+
+    }
+
+}
