f00b1aed2ff517df0b7b6b05c3ffb2027ee19867
Dinesh Borude <borudedinesh2@gmail.com>
2024-06-19 23:56:32 +0530

    Still Working With Stars

--- Problems/patterns/Triangle_Stars.java
+++ Problems/patterns/Triangle_Stars.java
@@ -4,7 +4,7 @@ public class Triangle_Stars {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 5; i >= 0; i--) {
 
             for (int j = 0; j < i+1; j++) {
 
