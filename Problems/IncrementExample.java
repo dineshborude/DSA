c9a10490a07ab4e930efc2cd5d9d79e0facf7992
Dinesh Borude <borudedinesh2@gmail.com>
2024-06-24 00:17:53 +0530

    operators

---
+++ Problems/IncrementExample.java
@@ -0,0 +1,12 @@
+public class IncrementExample {
+
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = a++;
+
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+    }
+
+}
