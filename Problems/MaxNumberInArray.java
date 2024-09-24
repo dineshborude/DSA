905478fdc913cdf182b3c8c5a0fdb59750b65e66
Dinesh Borude <borudedinesh2@gmail.com>
2024-09-25 01:42:18 +0530

    second max

--- Problems/MaxNumberInArray.java
+++ Problems/MaxNumberInArray.java
@@ -3,24 +3,29 @@ import java.util.Arrays;
 public class MaxNumberInArray {
 
     public static void main(String[] args) {
-        int[] num = {13,4,7,18,9,2};
+        int[] num = {23,4,7,180,9,90};
         System.out.println(Arrays.toString(num));
         int length = num.length;
         System.out.println("Length Of Given Array : "+length);
         int max = 0;
 
         for (int i = 0; i < length; i++) {
-            max = num[i];
-            for (int j = 0; j < length; j++) {
 
-                if (max <= num[j]) {
-                    max = num[j];
-                }
-
+            if (num[i] >= max) {
+                max = num[i];
             }
-
         }
+        int secondMax = 0;
+        for (int j = 0; j < length; j++)
+        {
+            if(num[j] < max && num[j] > secondMax) {
+                secondMax = num[j];
+            }
+        }
+
         System.out.println("Maximum In Array : "+max);
+        System.out.println("Second Maximum In Array : "+secondMax);
+
     }
 
 
--- Problems/TrafficLightExample.java
+++ Problems/TrafficLightExample.java
@@ -1,19 +1,10 @@
 public class TrafficLightExample {
     public static void main(String[] args) {
-        String lightColor = "Yellow";
-        boolean isDaytime = false;
+        String lightColor = "green";
+        boolean isDaytime = true;
         String action;
 
         switch (lightColor) {
-            case "Red":
-                action = isDaytime ? "Stop and wait for the light to turn green" : "Stop and turn on your headlights";
-                break;
-            case "Yellow":
-                action = isDaytime ? "Prepare to stop" : "Slow down and prepare to stop";
-                break;
-            case "Green":
-                action = isDaytime ? "Go ahead" : "Proceed with caution";
-                break;
             default:
                 action = "Unknown light color";
                 break;
